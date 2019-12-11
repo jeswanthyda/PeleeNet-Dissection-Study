a82e977d815e3e10bdf3614ec5e93b373fad29d8
Ruturaj-lab <rn2494@columbia.edu>
2019-12-11 05:46:15 +0000

    DenseNet

--- peleenet.py
+++ peleenet.py
@@ -1,27 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[12]:
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
-get_ipython().run_line_magic('load_ext', 'autoreload')
-get_ipython().run_line_magic('autoreload', '2')
-# Import modules
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import tensorflow as tf
-
-# In[16]:
-
-
-import pandas as pd
-import os
 from tensorflow.keras.layers import Input,Dense,Activation,GlobalAveragePooling2D,Conv2D,MaxPooling2D,Flatten,Dropout,BatchNormalization,AveragePooling2D,Concatenate
 from tensorflow.keras.models import Model
 from tensorflow.keras import regularizers
 from tensorflow.keras import initializers
+import tensorflow as tf
 
 
 def conv2d_bn(x, nb_filter, num_row, num_col,
@@ -31,32 +15,30 @@ def conv2d_bn(x, nb_filter, num_row, num_col,
                       strides=strides,
                       padding=padding,
                       kernel_regularizer=regularizers.l2(weight_decay))(x)
-    x = BatchNormalization(axis=-1, momentum=0.9997, scale=False)(x)
+    x = BatchNormalization()(x)
     x = Activation('relu')(x)
     
     return x
 
-
 def stem(x,num_init_channel=32):
     
-    conv1_output = conv2d_bn(x, num_init_channel, 3, 3,strides=(2,2))
-    branch_0 = conv2d_bn(conv1_output, int(num_init_channel/2), 1, 1)
-    branch_0 = conv2d_bn(branch_0, num_init_channel, 3, 3,strides=(2,2))
-    #print("the left branch is calculated")
+    conv1_output = conv2d_bn(x, 32, 3, 3,strides=(2,2))
+    branch_0 = conv2d_bn(conv1_output, 16, 1, 1)
+    branch_0 = conv2d_bn(branch_0, 32, 3, 3,strides=(2,2))
     branch_1 = MaxPooling2D(2, strides=(2,2))(conv1_output)
-    #print("the right branch is calulated")
     out = Concatenate()([branch_0, branch_1])
-    output = conv2d_bn(out, num_init_channel, 1, 1)
+    output = conv2d_bn(out, 32, 1, 1)
     
     return output
 
 def dense_block(x, num_block, bottleneck_width, k=32):
     
-    inter_channel = k*bottleneck_width
+    k = int(k / 2)
     output=x
     
     for index in range(num_block):
         #left channel     
+        inter_channel = int(k*bottleneck_width/4) * 4
         conv_branch_0 = conv2d_bn(output, inter_channel, 1, 1)
         conv_branch_0 = conv2d_bn(conv_branch_0, k, 3, 3)
         # right channel
@@ -68,16 +50,14 @@ def dense_block(x, num_block, bottleneck_width, k=32):
 
     return output
 
-
 def transition_block(x, output_channel, is_avgpool=True):
     
-    conv0 = conv2d_bn(x, output_channel, 1, 1, strides=(2,2))
+    conv0 = conv2d_bn(x, output_channel, 1, 1, strides=(1,1))
     if is_avgpool:
         output=AveragePooling2D((2,2),strides=(2,2))(conv0)
     else:
         output=conv0
     return output
-
 
 def classification_layer(x, n_classes=1000):
     
@@ -85,8 +65,7 @@ def classification_layer(x, n_classes=1000):
     x = Dense(n_classes, activation="softmax")(x)
     return x
 
-
-def PeleeNet(input_shape=(224,224,3),use_stem_block=True, num_init_channel=3, k=32, block_config=[3,4,8,6], out_layers = [128,256,512,704],bottleneck_width=[2,2,4,4],n_classes=1000):
+def PeleeNet(input_shape=(224,224,3),use_stem_block=True, num_init_channel=3, k=32, block_config=[3,4,8,6], out_layers = [128,256,512,704],bottleneck_width=[1,2,4,4],n_classes=1000):
     
     inputs = Input(shape=input_shape)
     x=stem(inputs,num_init_channel) if use_stem_block else inputs
